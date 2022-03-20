3bb626fa27fc3df5660a49f34d3aaa7d19c0adf6
colin <colin@colindeMacBook-Pro.local>
2022-03-20 11:42:37 +0800

    new image

--- ci/build-test.sh
+++ ci/build-test.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 set -xe
-# git clone --branch v1.9.2 https://github.com/gabime/spdlog.git && cd spdlog && mkdir build && cd build && cmake .. && make -j && make install
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SRCPATH=$(cd $SCRIPTPATH/..; pwd -P)
 NPROC=$(nproc || grep -c ^processor /proc/cpuinfo)
 
-# if [ -d "$SRCPATH/Protocol" ]; then
-#   cd "$SRCPATH/Protocol"
-#   chmod -R 755 scripts
-#   ./scripts/generate_cpp.sh
-#   cd -
-# fi
+if [ -d "$SRCPATH/Protocol" ]; then
+  cd "$SRCPATH/Protocol"
+  chmod -R 755 scripts
+  ./scripts/generate_cpp.sh
+  cd -
+fi
 
 build_dir="$SRCPATH/build_"
 mkdir -p $build_dir && cd $build_dir
